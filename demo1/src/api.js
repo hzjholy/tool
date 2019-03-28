818ea258d2f6d22ea7f48ab064ff23a9d6f3fc44
hzjholy <hzjholy@163.com>
2019-03-28 15:08:24 +0800

    webpack demo1

---
+++ demo1/src/api.js
@@ -0,0 +1,9 @@
+var a = 1;
+
+var b = 2;
+
+module.exports = function(){
+	var c = 3;
+	return a + b;
+
+}
---
+++ demo1/src/index.js
@@ -0,0 +1,6 @@
+var api = require('./api.js')
+
+window.onload = function(){
+	var result = api();
+	console.log('result ==>',result)
+}
